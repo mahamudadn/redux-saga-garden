f835431909db12f320ddf965498dd2b7c998e75b
Mahamud Aden <mahamudadn@gmail.com>
2023-06-14 20:30:22 -0500

    worked on plantlist and index to render on the Dom

--- src/components/PlantList/PlantList.js
+++ src/components/PlantList/PlantList.js
@@ -5,17 +5,24 @@ import { useDispatch, useSelector} from 'react-redux';
 function PlantList() {
     const dispatch = useDispatch();
 
-    const reduxState = useSelector(store => store);
+    const reduxState = useSelector(store => store.plantList);
 
     useEffect(() => {
         console.log('component did mount');
         // dispatch an action to request the plantList from the API
+        dispatch({type:'GET_PLANT'})
     }, []); 
 
     return (
         <div>
             <h3>This is the plant list</h3>
-            <pre>{JSON.stringify(reduxState)}</pre>
+            {/* <pre>{JSON.stringify(reduxState)}</pre> */}
+            {reduxState.map((plant, i) => {
+                return <div>
+                    <li key={i}>{plant.name}</li>
+                <button onClick={() => dispatch({type: 'DELETE_PLANT' , payload: plant.id})}>Delete</button>
+                </div>
+            })}
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -5,29 +5,87 @@ import { Provider } from 'react-redux';
 
 import App from './App';
 import logger from 'redux-logger';
+import axios from 'axios';
+import createSagaMiddleware from 'redux-saga';
+import { takeLatest, put } from 'redux-saga/effects';
 
 // this startingPlantArray should eventually be removed
-const startingPlantArray = [
-  { id: 1, name: 'Rose' },
-  { id: 2, name: 'Tulip' },
-  { id: 3, name: 'Oak' }
-];
+// const startingPlantArray = [
+//   { id: 1, name: 'Rose' },
+//   { id: 2, name: 'Tulip' },
+//   { id: 3, name: 'Oak' }
+// ];
 
-const plantList = (state = startingPlantArray, action) => {
+const plantList = (state = [], action) => {
   switch (action.type) {
     case 'ADD_PLANT':
-      return [ ...state, action.payload ]
+      return  action.payload 
     default:
       return state;
   }
 };
 
+// Genarator Function
+
+function* getPlant () {
+  try {
+
+    const plantResponse = yield axios.get('/api/plant')
+    // after server responds, then this generator function/saga continues.
+      yield put ({type:'ADD_PLANT', payload: plantResponse.data})
+  }
+  catch(err) {
+    console.log('error getting plants', err);
+  }
+}
+
+
+function* postPlant (action) {
+
+  try {
+      yield axios.post('/api/plant', action.payload)
+
+      yield put ({type:'GET_PLANT'})
+
+  }
+  catch(error) {
+    console.log('error', error);
+  }
+
+}
+
+function* deletePlant (action) {
+  try { yield axios.delete(`/api/plant/${action.payload}`)
+  yield put ({type: 'GET_PLANT'})
+
+}
+catch(error) {
+  console.log('Error Deleting Plant', error);
+}
+
+}
+// Create sagaMiddleware
+const sagaMiddleware = createSagaMiddleware();
+
+// Create the rootSaga generator function
+function* rootSaga() {
+  yield takeLatest('GET_PLANT',getPlant)
+  yield takeLatest('POST_PLANT', postPlant)
+  yield takeLatest('DELETE_PLANT', deletePlant)
+}
+
 const store = createStore(
   combineReducers({ plantList }),
 
 
-  applyMiddleware(logger),
+  applyMiddleware(sagaMiddleware, logger),
 );
+
+// Pass rootSaga into our sagaMiddleware
+
+sagaMiddleware.run(rootSaga)
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
